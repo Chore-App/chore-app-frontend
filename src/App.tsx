6bfaea2df608b8a5168e3f7e22e6e79fc1971ee2
Shashank Vasanth <shashank@Shashanks-MBP.lan>
2024-02-02 23:20:16 -0500

    1-setup-boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+// src/App.tsx
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, CounterState, countSelector} from './CounterSlice';
+import { RootState } from './Store';
 
+const App: React.FC = () => {
+  const count = useSelector((state:RootState) => state.counter.count);
+  const dispatch = useDispatch();
 
-function App() {
   return (
-    <div className="App">
-     Hello world!
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Store.ts
@@ -0,0 +1,15 @@
+// src/store.ts
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from './CounterSlice';
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+
+export default store;
